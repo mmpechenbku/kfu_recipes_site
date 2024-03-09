db9eee54bf03e1d08939139fbbb4bc64f439dfdb
mmpechenbku <mmpechenbku@gmail.com>
2024-03-10 01:03:23 +0300

    added registration, base and home pages

--- web/urls.py
+++ web/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from web.views import home_view, registration_view
 
 urlpatterns = [
-    path('/', ),
+    path('', home_view, name='home'),
+    path('sign_up', registration_view, name='sign_up'),
 ]
